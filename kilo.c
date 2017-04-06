a8c18389620e2c4535170fe601af6c980c8fa489
William Roe <git@wjlr.org.uk>
2017-04-06 18:27:07 +0100

    Refactor input

--- kilo.c
+++ kilo.c
@@ -40,25 +40,33 @@ void enableRawMode() {
   }
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) {
+      die("read");
+      ;
+    }
+  }
+  return c;
+}
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+  case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
+}
+
 int main() {
   enableRawMode();
 
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-      die("read");
-    }
-    if (c == CTRL_KEY('q')) {
-      break;
-    }
-    if (isprint(c)) {
-      printf("%d ('%c')\r\n", c, c);
-    } else {
-      printf("%d\r\n", c);
-    }
-    if (c == 'q') {
-      break;
-    }
+    editorProcessKeypress();
   }
   return 0;
 }
