3e61f176e46c73e357d8c189af7d080ade707267
William Roe <git@wjlr.org.uk>
2017-04-06 18:34:56 +0100

    Clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -10,6 +10,9 @@
 struct termios orig_termios;
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -62,6 +65,8 @@ void editorProcessKeypress() {
 
   switch (c) {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
   }
