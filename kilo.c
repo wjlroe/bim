3411d825524c464f30ffd49ca658f91523742669
William Roe <git@wjlr.org.uk>
2017-04-06 18:02:59 +0100

    Fix Ctrl-m

--- kilo.c
+++ kilo.c
@@ -14,7 +14,7 @@ void enableRawMode() {
 
   struct termios raw = orig_termios;
 
-  raw.c_iflag &= ~(IXON);
+  raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
