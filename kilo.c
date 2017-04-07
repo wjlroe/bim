9b1a45a4e154f5ab34cc0bc889e659c7454e8164
William Roe <git@wjlr.org.uk>
2017-04-07 09:00:26 +0100

    Move cursor with wasd

--- kilo.c
+++ kilo.c
@@ -179,6 +179,23 @@ void editorRefreshScreen() {
   abFree(&ab);
 }
 
+void editorMoveCursor(char key) {
+  switch (key) {
+  case 'a':
+    E.cx--;
+    break;
+  case 'd':
+    E.cx++;
+    break;
+  case 'w':
+    E.cy--;
+    break;
+  case 's':
+    E.cy++;
+    break;
+  }
+}
+
 void editorProcessKeypress() {
   char c = editorReadKey();
 
@@ -187,6 +204,13 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+
+  case 'w':
+  case 's':
+  case 'a':
+  case 'd':
+    editorMoveCursor(c);
     break;
   }
 }
