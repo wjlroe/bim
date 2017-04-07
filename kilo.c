97ac1f5ad4d676f2f8ce637450db7c81dcc46a54
William Roe <git@wjlr.org.uk>
2017-04-07 09:05:23 +0100

    Arrow keys to move the cursor

--- kilo.c
+++ kilo.c
@@ -61,7 +61,34 @@ char editorReadKey() {
       die("read");
     }
   }
-  return c;
+
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+      return '\x1b';
+    }
+
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+      case 'A':
+        return 'w';
+      case 'B':
+        return 's';
+      case 'C':
+        return 'd';
+      case 'D':
+        return 'a';
+      }
+    }
+
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 int getCursorPosition(int *rows, int *cols) {
