2c29053a867bb91292c7aaa2ac627348d3564c08
William Roe <git@wjlr.org.uk>
2017-04-10 08:43:09 +0100

    Delete key does nothing

--- kilo.c
+++ kilo.c
@@ -15,6 +15,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -92,6 +93,8 @@ int editorReadKey() {
           switch (seq[1]) {
             case '1':
               return HOME_KEY;
+            case '3':
+              return DEL_KEY;
             case '4':
               return END_KEY;
             case '5':
