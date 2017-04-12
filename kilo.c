b2d966defe3f053b13895b940772112485994167
William Roe <git@wjlr.org.uk>
2017-04-12 08:55:02 +0100

    Status bar

--- kilo.c
+++ kilo.c
@@ -44,6 +44,7 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow* row;
+  char* filename;
   struct termios orig_termios;
 };
 
@@ -257,6 +258,9 @@ void editorAppendRow(char* s, size_t len) {
 }
 
 void editorOpen(char* filename) {
+  free(E.filename);
+  E.filename = strdup(filename);
+
   FILE* fp = fopen(filename, "r");
   if (!fp) {
     die("fopen");
@@ -353,10 +357,30 @@ void editorDrawRows(struct abuf* ab) {
     }
 
     abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 2);
+    abAppend(ab, "\r\n", 2);
+  }
+}
+
+void editorDrawStatusBar(struct abuf* ab) {
+  abAppend(ab, "\x1b[7m", 4);
+  char status[80], rstatus[80];
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+                     E.filename ? E.filename : "[No Name]", E.numrows);
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+  if (len > E.screencols) {
+    len = E.screencols;
+  }
+  abAppend(ab, status, len);
+  while (len < E.screencols) {
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
     }
   }
+  abAppend(ab, "\x1b[m", 3);
 }
 
 void editorRefreshScreen() {
@@ -368,6 +392,7 @@ void editorRefreshScreen() {
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
+  editorDrawStatusBar(&ab);
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
@@ -470,10 +495,12 @@ void initEditor() {
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.filename = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
     die("getWindowSize");
   }
+  E.screenrows -= 1;
 }
 
 int main(int argc, char* argv[]) {
