dd597b0ea6e5e7757be6e29c112f41bf9f9a5faa
William Roe <git@wjlr.org.uk>
2017-04-12 17:58:24 +0100

    Warn before quitting when file unsaved

--- kilo.c
+++ kilo.c
@@ -17,6 +17,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 #define CTRL_KEY(k) ((k)&0x1f)
 
 enum editorKey {
@@ -544,6 +545,8 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
 
   switch (c) {
@@ -552,6 +555,14 @@ void editorProcessKeypress() {
       break;
 
     case CTRL_KEY('q'):
+      if (E.unsaved && quit_times > 0) {
+        editorSetStatusMessage(
+            "WARNING!!! File has unsaved changes. Press Ctrl-Q %d more times "
+            "to quit.",
+            quit_times);
+        quit_times--;
+        return;
+      }
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
@@ -606,6 +617,8 @@ void editorProcessKeypress() {
       editorInsertChar(c);
       break;
   }
+
+  quit_times = KILO_QUIT_TIMES;
 }
 
 void initEditor() {
