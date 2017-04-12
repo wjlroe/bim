eaab749313edd656680fb5583fd3f4476475275a
William Roe <git@wjlr.org.uk>
2017-04-12 08:38:40 +0100

    End to go to end of line

--- kilo.c
+++ kilo.c
@@ -434,7 +434,9 @@ void editorProcessKeypress() {
       break;
 
     case END_KEY:
-      E.cx = E.screencols - 1;
+      if (E.cy < E.numrows) {
+        E.cx = E.row[E.cy].size;
+      }
       break;
 
     case PAGE_UP:
