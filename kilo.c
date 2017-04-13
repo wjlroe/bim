f13aecce21f762611aeab0e28ea117063acbde27
William Roe <git@wjlr.org.uk>
2017-04-13 08:23:30 +0100

    Backspacing at the beginning of the line

--- kilo.c
+++ kilo.c
@@ -268,6 +268,21 @@ void editorAppendRow(char* s, size_t len) {
   E.unsaved++;
 }
 
+void editorFreeRow(erow* row) {
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows) {
+    return;
+  }
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.unsaved++;
+}
+
 void editorRowInsertChar(erow* row, int at, int c) {
   if (at < 0 || at > row->size) {
     at = row->size;
@@ -276,6 +291,15 @@ void editorRowInsertChar(erow* row, int at, int c) {
   memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
   row->size++;
   row->chars[at] = c;
+  editorUpdateRow(row);
+  E.unsaved++;
+}
+
+void editorRowAppendString(erow* row, char* s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
   editorUpdateRow(row);
   E.unsaved++;
 }
@@ -302,11 +326,19 @@ void editorDelChar() {
   if (E.cy == E.numrows) {
     return;
   }
+  if (E.cx == 0 && E.cy == 0) {
+    return;
+  }
 
   erow* row = &E.row[E.cy];
   if (E.cx > 0) {
     editorRowDelChar(row, E.cx - 1);
     E.cx--;
+  } else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
   }
 }
 
