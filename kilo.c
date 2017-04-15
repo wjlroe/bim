588300c5fc081d84967b8623e61a1164118c10a7
William Roe <git@wjlr.org.uk>
2017-04-15 01:54:36 +0100

    Save as

--- kilo.c
+++ kilo.c
@@ -59,6 +59,8 @@ struct editorConfig {
 struct editorConfig E;
 
 void editorSetStatusMessage(const char* fmt, ...);
+void editorRefreshScreen();
+char* editorPrompt(char* prompt);
 
 void die(const char* s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -407,7 +409,11 @@ void editorOpen(char* filename) {
 
 void editorSave() {
   if (E.filename == NULL) {
-    return;
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL) {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
   }
 
   int len;
@@ -576,6 +582,38 @@ void editorSetStatusMessage(const char* fmt, ...) {
   vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
   va_end(ap);
   E.statusmsg_time = time(NULL);
+}
+
+char* editorPrompt(char* prompt) {
+  size_t bufsize = 128;
+  char* buf = malloc(bufsize);
+
+  size_t buflen = 0;
+  buf[0] = '\0';
+
+  while (1) {
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+
+    int c = editorReadKey();
+    if (c == '\x1b') {
+      editorSetStatusMessage("");
+      free(buf);
+      return NULL;
+    } else if (c == '\r') {
+      if (buflen != 0) {
+        editorSetStatusMessage("");
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) {
+      if (buflen == bufsize - 1) {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
 }
 
 void editorMoveCursor(int key) {
