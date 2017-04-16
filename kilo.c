8fcf27fff7989ec45d29dfbb9982b6822a812f95
William Roe <git@wjlr.org.uk>
2017-04-16 02:01:13 +0100

    Incremental search

--- kilo.c
+++ kilo.c
@@ -60,7 +60,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char* fmt, ...);
 void editorRefreshScreen();
-char* editorPrompt(char* prompt);
+char* editorPrompt(char* prompt, void (*callback)(char*, int));
 
 void die(const char* s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -425,7 +425,7 @@ void editorOpen(char* filename) {
 
 void editorSave() {
   if (E.filename == NULL) {
-    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
     if (E.filename == NULL) {
       editorSetStatusMessage("Save aborted");
       return;
@@ -453,9 +453,8 @@ void editorSave() {
   editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
-void editorFind() {
-  char* query = editorPrompt("Search: %s (ESC to cancel)");
-  if (query == NULL) {
+void editorFindCallback(char* query, int key) {
+  if (key == '\r' || key == '\x1b') {
     return;
   }
 
@@ -470,8 +469,14 @@ void editorFind() {
       break;
     }
   }
+}
 
-  free(query);
+void editorFind() {
+  char* query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+
+  if (query) {
+    free(query);
+  }
 }
 
 struct abuf {
@@ -621,7 +626,7 @@ void editorSetStatusMessage(const char* fmt, ...) {
   E.statusmsg_time = time(NULL);
 }
 
-char* editorPrompt(char* prompt) {
+char* editorPrompt(char* prompt, void (*callback)(char*, int)) {
   size_t bufsize = 128;
   char* buf = malloc(bufsize);
 
@@ -639,11 +644,17 @@ char* editorPrompt(char* prompt) {
       }
     } else if (c == '\x1b') {
       editorSetStatusMessage("");
+      if (callback) {
+        callback(buf, c);
+      }
       free(buf);
       return NULL;
     } else if (c == '\r') {
       if (buflen != 0) {
         editorSetStatusMessage("");
+        if (callback) {
+          callback(buf, c);
+        }
         return buf;
       }
     } else if (!iscntrl(c) && c < 128) {
@@ -653,6 +664,10 @@ char* editorPrompt(char* prompt) {
       }
       buf[buflen++] = c;
       buf[buflen] = '\0';
+    }
+
+    if (callback) {
+      callback(buf, c);
     }
   }
 }
