bf67543d9f2c28ca6fa59cc2e54398d418545af6
William Roe <git@wjlr.org.uk>
2017-04-18 18:52:36 +0100

    Colourful digits (digits are red)

--- kilo.c
+++ kilo.c
@@ -587,7 +587,17 @@ void editorDrawRows(struct abuf* ab) {
       if (len > E.screencols) {
         len = E.screencols;
       }
-      abAppend(ab, &E.row[filerow].render[E.coloff], len);
+      char* c = &E.row[filerow].render[E.coloff];
+      int j;
+      for (j = 0; j < len; j++) {
+        if (isdigit(c[j])) {
+          abAppend(ab, "\x1b[31m", 5);
+          abAppend(ab, &c[j], 1);
+          abAppend(ab, "\x1b[39m", 5);
+        } else {
+          abAppend(ab, &c[j], 1);
+        }
+      }
     }
 
     abAppend(ab, "\x1b[K", 3);
