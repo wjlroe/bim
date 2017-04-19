209d58851b111c31c450e7235ff8e52bb87ec7aa
William Roe <git@wjlr.org.uk>
2017-04-19 08:40:44 +0100

    Colourful strings

--- kilo.c
+++ kilo.c
@@ -21,6 +21,7 @@
 #define CTRL_KEY(k) ((k)&0x1f)
 
 #define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
 
 enum editorKey {
   BACKSPACE = 127,
@@ -37,6 +38,7 @@ enum editorKey {
 
 enum editorHighlight {
   HL_NORMAL = 0,
+  HL_STRING,
   HL_NUMBER,
   HL_MATCH,
 };
@@ -77,9 +79,7 @@ struct editorConfig E;
 char* C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 
 struct editorSyntax HLDB[] = {
-    {
-        "c", C_HL_extensions, HL_HIGHLIGHT_NUMBERS,
-    },
+    {"c", C_HL_extensions, HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
 };
 
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
@@ -253,11 +253,36 @@ void editorUpdateSyntax(erow* row) {
   }
 
   int prev_sep = 1;
+  int in_string = 0;
 
   int i;
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+      if (in_string) {
+        row->hl[i] = HL_STRING;
+        if (c == '\\' && i + 1 < row->rsize) {
+          row->hl[i + 1] = HL_STRING;
+          i += 2;
+          continue;
+        }
+        if (c == in_string) {
+          in_string = 0;
+        }
+        i++;
+        prev_sep = 1;
+        continue;
+      } else {
+        if (c == '"' || c == '\'') {
+          in_string = c;
+          row->hl[i] = HL_STRING;
+          i++;
+          continue;
+        }
+      }
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
       if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
@@ -305,6 +330,8 @@ void editorSelectSyntaxHighlight() {
 
 int editorSyntaxToColor(int hl) {
   switch (hl) {
+    case HL_STRING:
+      return 35;
     case HL_NUMBER:
       return 31;
     case HL_MATCH:
