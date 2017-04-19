83cd11b50c2f4dafcc09e8b347dc04d197b97722
William Roe <git@wjlr.org.uk>
2017-04-19 08:06:00 +0100

    Highlight search results in blue

--- kilo.c
+++ kilo.c
@@ -36,6 +36,7 @@ enum editorKey {
 enum editorHighlight {
   HL_NORMAL = 0,
   HL_NUMBER,
+  HL_MATCH,
 };
 
 typedef struct erow {
@@ -236,6 +237,8 @@ int editorSyntaxToColor(int hl) {
   switch (hl) {
     case HL_NUMBER:
       return 31;
+    case HL_MATCH:
+      return 34;
     default:
       return 37;
   }
@@ -488,6 +491,15 @@ void editorFindCallback(char* query, int key) {
   static int last_match = -1;
   static int direction = 1;
 
+  static int saved_hl_line;
+  static char* saved_hl = NULL;
+
+  if (saved_hl) {
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
+
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
     direction = 1;
@@ -521,6 +533,11 @@ void editorFindCallback(char* query, int key) {
       E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      saved_hl_line = current;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
+      memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
   }
