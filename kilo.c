a88bab93145cd1e48fbd9909d8155fc426515157
William Roe <git@wjlr.org.uk>
2017-04-20 18:22:55 +0100

    Highlight comments

--- kilo.c
+++ kilo.c
@@ -38,6 +38,7 @@ enum editorKey {
 
 enum editorHighlight {
   HL_NORMAL = 0,
+  HL_COMMENT,
   HL_STRING,
   HL_NUMBER,
   HL_MATCH,
@@ -46,6 +47,7 @@ enum editorHighlight {
 struct editorSyntax {
   char* filetype;
   char** filematch;
+  char* singleline_comment_start;
   int flags;
 };
 
@@ -79,7 +81,7 @@ struct editorConfig E;
 char* C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 
 struct editorSyntax HLDB[] = {
-    {"c", C_HL_extensions, HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
+    {"c", C_HL_extensions, "//", HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
 };
 
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
@@ -252,13 +254,23 @@ void editorUpdateSyntax(erow* row) {
     return;
   }
 
+  char* scs = E.syntax->singleline_comment_start;
+  int scs_len = scs ? strlen(scs) : 0;
+
   int prev_sep = 1;
   int in_string = 0;
 
-  int i;
+  int i = 0;
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (scs_len && !in_string) {
+      if (!strncmp(&row->render[i], scs, scs_len)) {
+        memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+        break;
+      }
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
       if (in_string) {
@@ -330,6 +342,8 @@ void editorSelectSyntaxHighlight() {
 
 int editorSyntaxToColor(int hl) {
   switch (hl) {
+    case HL_COMMENT:
+      return 36;
     case HL_STRING:
       return 35;
     case HL_NUMBER:
