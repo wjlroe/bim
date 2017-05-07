07ec92ec498c2440b77de97a5deb8d22ab0be2ce
William Roe <git@wjlr.org.uk>
2017-05-07 02:58:09 +0100

    C++ win32: timeout+error handling
    Windows doesn't seem to have a read() call with timeout, so we have to use
    WaitForSingleObject and then read using ReadConsoleInput (ReadConsole doesn't
    seem to work for this?). This is a more complex API than just reading
    characters, but in the end it might be more useful.
    
    Also in the build script, it ignores the subsystem argument and it doesn't seem
    to affect anything, so just remove it.

--- src/build.bat
+++ src/build.bat
@@ -4,5 +4,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 
 IF NOT EXIST build mkdir build
 pushd build
-cl -FC -Zi -EHsc ..\src\main.cpp /SUBSYSTEM:CONSOLE
+cl -FC -Zi -EHsc ..\src\main.cpp
 popd
--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,87 @@
 #include <iostream>
 
 static HANDLE stdIn;
-static DWORD ORIG_MODE;
+static HANDLE stdOut;
+static DWORD ORIG_INPUT_MODE;
+static DWORD ORIG_OUTPUT_MODE;
+
+#define NUM_EVENTS 1
+
+void die(const char* s)
+{
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode()
 {
-    std::cout << "gonna to disable raw mode...";
-    SetConsoleMode(stdIn, ORIG_MODE);
-    std::cout << "done\n";
+    SetConsoleMode(stdIn, ORIG_INPUT_MODE);
+    SetConsoleMode(stdOut, ORIG_OUTPUT_MODE);
 }
 
 int main(int argc, char* argv[])
 {
     stdIn = GetStdHandle(STD_INPUT_HANDLE);
+    stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    std::cout << "gonna get current console mode\n";
-    if (!GetConsoleMode(stdIn, &ORIG_MODE)) {
-        return 1;
+    if (!GetConsoleMode(stdIn, &ORIG_INPUT_MODE)) {
+        die("failed to get stdin mode");
+    }
+
+    if (!GetConsoleMode(stdOut, &ORIG_OUTPUT_MODE)) {
+        die("failed to get stdout mode");
     }
 
     atexit(disableRawMode);
 
-    std::cout << "gonna set non-echo and non line input\n";
-    DWORD rawMode = ORIG_MODE;
-    rawMode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT);
-    if (!SetConsoleMode(stdIn, rawMode)) {
-        return 2;
+    {
+        DWORD rawMode = ORIG_INPUT_MODE;
+        rawMode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT);
+        if (!SetConsoleMode(stdIn, rawMode)) {
+            die("failed to set stdin mode");
+        }
     }
 
-    std::cout << "time to read some chars\n";
+    {
+        DWORD enable_virtual_terminal_processing = 4;
+        DWORD disable_newline_auto_return = 8;
 
-    char buf[] = {0};
-    DWORD num = 0;
+        DWORD rawMode = ORIG_OUTPUT_MODE;
+        rawMode |= (disable_newline_auto_return | enable_virtual_terminal_processing);
+        if (!SetConsoleMode(stdOut, rawMode)) {
+            die("failed to set stdout mode");
+        }
+    }
+
     bool running = true;
     while (running) {
-        if (!ReadConsole(stdIn, &buf, 1, &num, NULL)) {
-            return 3;
+        char buf[NUM_EVENTS] = {0};
+        DWORD num = 0;
+
+        DWORD waiting = WaitForSingleObject(stdIn, 1000);
+        if (waiting == WAIT_OBJECT_0) {
+            INPUT_RECORD input[NUM_EVENTS];
+            DWORD numEventsRead;
+            if (!ReadConsoleInput(stdIn, input, NUM_EVENTS, &numEventsRead)) {
+                die("failed to ReadConsoleInput");
+            }
+
+            for (int i = 0; i < NUM_EVENTS; i++) {
+                switch (input[i].EventType) {
+                case KEY_EVENT: {
+                    KEY_EVENT_RECORD record = input[i].Event.KeyEvent;
+                    if (record.bKeyDown) {
+                        buf[i] = record.uChar.AsciiChar;
+                    }
+                } break;
+                }
+            }
         }
 
-        printf("read a char: %c\n", buf[0]);
+        char output[64];
+        DWORD numChars = sprintf(output, "read a char: %c\r\n", buf[0]);
+        DWORD writtenChars;
+        WriteConsole(stdOut, output, numChars, &writtenChars, NULL);
 
         if (buf[0] == 'q') {
             running = false;
