ef52fd5ef94e3a960826b7b11c0e1261f272004d
William Roe <git@wjlr.org.uk>
2017-05-08 08:52:12 +0100

    C++: refactor input

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,19 @@ static DWORD ORIG_INPUT_MODE;
 static DWORD ORIG_OUTPUT_MODE;
 
 #define NUM_EVENTS 1
-#define CTRL_KEY(k) ((k) & 0x1f)
+#define CTRL_KEY(k) ((k)&0x1f)
 
-void die(const char* s)
-{
+void die(const char* s) {
     perror(s);
     exit(1);
 }
 
-void disableRawMode()
-{
+void disableRawMode() {
     SetConsoleMode(stdIn, ORIG_INPUT_MODE);
     SetConsoleMode(stdOut, ORIG_OUTPUT_MODE);
 }
 
-int main(int argc, char* argv[])
-{
+void enableRawMode() {
     stdIn = GetStdHandle(STD_INPUT_HANDLE);
     stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -38,7 +35,8 @@ int main(int argc, char* argv[])
 
     {
         DWORD rawMode = ORIG_INPUT_MODE;
-        rawMode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT);
+        rawMode &=
+            ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT);
         if (!SetConsoleMode(stdIn, rawMode)) {
             die("failed to set stdin mode");
         }
@@ -49,44 +47,52 @@ int main(int argc, char* argv[])
         DWORD disable_newline_auto_return = 8;
 
         DWORD rawMode = ORIG_OUTPUT_MODE;
-        rawMode |= (disable_newline_auto_return | enable_virtual_terminal_processing);
+        rawMode |=
+            (disable_newline_auto_return | enable_virtual_terminal_processing);
         if (!SetConsoleMode(stdOut, rawMode)) {
             die("failed to set stdout mode");
         }
     }
+}
 
-    bool running = true;
-    while (running) {
-        char buf[NUM_EVENTS] = {0};
-        DWORD num = 0;
+char readKey() {
+    char character = '\0';
 
-        DWORD waiting = WaitForSingleObject(stdIn, 1000);
-        if (waiting == WAIT_OBJECT_0) {
-            INPUT_RECORD input[NUM_EVENTS];
-            DWORD numEventsRead;
-            if (!ReadConsoleInput(stdIn, input, NUM_EVENTS, &numEventsRead)) {
-                die("failed to ReadConsoleInput");
-            }
+    DWORD waiting = WaitForSingleObject(stdIn, 1000);
+    if (waiting == WAIT_OBJECT_0) {
+        INPUT_RECORD input[1];
+        DWORD numEventsRead;
+        if (!ReadConsoleInput(stdIn, input, NUM_EVENTS, &numEventsRead)) {
+            die("failed to ReadConsoleInput");
+        }
 
-            for (int i = 0; i < NUM_EVENTS; i++) {
-                switch (input[i].EventType) {
+        if (numEventsRead == 1) {
+            switch (input[0].EventType) {
                 case KEY_EVENT: {
-                    KEY_EVENT_RECORD record = input[i].Event.KeyEvent;
+                    KEY_EVENT_RECORD record = input[0].Event.KeyEvent;
                     if (record.bKeyDown) {
-                        buf[i] = record.uChar.AsciiChar;
+                        character = record.uChar.AsciiChar;
                     }
                 } break;
-                }
             }
         }
+    }
 
-        char output[64];
-        DWORD numChars = sprintf(output, "read a char: %c\r\n", buf[0]);
-        DWORD writtenChars;
-        WriteConsole(stdOut, output, numChars, &writtenChars, NULL);
+    return character;
+}
 
-        if (buf[0] == CTRL_KEY('q')) {
-            running = false;
-        }
+void processKeyPress() {
+    char c = readKey();
+
+    if (c == CTRL_KEY('q')) {
+        exit(0);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    enableRawMode();
+
+    while (1) {
+        processKeyPress();
     }
 }
