13c75995abfaaa5c7234ab4d30274aae3354276c
William Roe <git@wjlr.org.uk>
2017-06-21 00:50:22 +0100

    Improved rustfmt installer for CI

--- scripts/prepare_ci.sh
+++ scripts/prepare_ci.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
-RUSTFMT_VERSION="0.8.4"
+set -x
+
+RUSTFMT_VERSION="0.9.0"
+
+unset CARGO_HOME
 
 install_rustfmt() {
   cargo install --force --vers "${RUSTFMT_VERSION}" rustfmt
 }
 
+get_version() {
+  rv=$(rustfmt --version)
+  [[ "$rv" =~ ([0-9\.]+) ]] && echo "${BASH_REMATCH[1]}"
+}
+
 which rustfmt >/dev/null || install_rustfmt
 
-installed_rustfmt_version=$(rustfmt  --version | cut -f 1 -d ' ')
+installed_rustfmt_version=$(get_version)
 
 if [ "x${installed_rustfmt_version}x" != "x${RUSTFMT_VERSION}x" ]; then
   install_rustfmt
 fi
 
-installed_rustfmt_version=$(rustfmt  --version | cut -f 1 -d ' ')
+installed_rustfmt_version=$(get_version)
 
 if [ "x${installed_rustfmt_version}x" != "x${RUSTFMT_VERSION}x" ]; then
   echo "Wanted rustfmt ${RUSTFMT_VERSION} but we have ${installed_rustfmt_version}!"
