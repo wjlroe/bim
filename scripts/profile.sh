f8f82022b43d887d18dace7e75d238867104a295
William Roe <git@wjlr.org.uk>
2019-09-26 19:12:50 +0100

    More flexible profile script, pass args through

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# TODO: parameterise debug/release build
-# TODO: pass extra args straight through rather than baking in
+TARGET_DIR="debug"
+BIM_ARGS=$*
+if [[ "$1" == "release"  ||  "$1" == "debug" ]]; then
+        TARGET_DIR=$1
+        BIM_ARGS=${*:2}
+fi
 
-valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --simulate-cache=yes target/debug/bim -- testfiles/kilo-unix.c
+valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --simulate-cache=yes target/${TARGET_DIR}/bim -- ${BIM_ARGS}
