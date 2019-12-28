728779f6bf98c6cd753eb6aa184bd69d99859fe6
William Roe <git@wjlr.org.uk>
2019-12-28 12:31:58 +0000

    Faster cursor blinking interval

--- src/gui/gui_pane.rs
+++ src/gui/gui_pane.rs
@@ -26,6 +26,7 @@ const STATUS_UNFOCUS_FG: [f32; 4] = [0.8, 0.8, 0.8, 1.0];
 const CURSOR_FOCUSED_BG: [f32; 3] = [250.0 / 256.0, 250.0 / 256.0, 250.0 / 256.0];
 const CURSOR_UNFOCUS_BG: [f32; 3] = [150.0 / 256.0, 150.0 / 256.0, 150.0 / 256.0];
 const OTHER_CURSOR_BG: [f32; 3] = [255.0 / 256.0, 165.0 / 256.0, 0.0];
+const CURSOR_BLINK_INTERVAL: u64 = 500;
 
 pub struct GuiPane<'a> {
     other_cursor: Option<Cursor>,
@@ -68,7 +69,7 @@ impl<'a> Default for GuiPane<'a> {
             left_padding: 12.0,
             row_offset: 0.0,
             col_offset: 0.0,
-            cursor_animation: Animation::new(Duration::new(1, 0)),
+            cursor_animation: Animation::new(Duration::from_millis(CURSOR_BLINK_INTERVAL)),
         }
     }
 }
